6bc4edafd9d76914e3cbd24ca397d69ab204c2ef
Eben Fuentes <eben.fuentes@gmail.com>
2026-01-29 06:48:51 -0800

    added interactive map controls and boundaries to the map.

--- src/components/CampusMap.vue
+++ src/components/CampusMap.vue
@@ -14,15 +14,31 @@ const toggleBuildings = () => {
 }
 
 onMounted(() => {
+    
+    const bounds = [
+        [-118.1888, 34.0616], // Southwest coordinates
+        [-118.1561, 34.0769] // Northeast coordinates
+    ]
+
     map.value = new maplibregl.Map({
         container: 'map',
         style: 'https://tiles.openfreemap.org/styles/liberty',
         center: [-118.16848, 34.06639],
         zoom: 16,
-        maplibreLogo: true
+        maplibreLogo: true,
+        maxBounds: bounds
     });
 
+    
+
     map.value.on('load', () => {
+
+        map.value.addControl(new maplibregl.NavigationControl({
+        visualizePitch: true,
+        visualizeRoll: true,
+        showZoom: true,
+        showCompass: true
+    }));
         map.value.getCanvas().style.cursor = 'crosshair'
 
         map.value.addSource('buildings', {
@@ -81,7 +97,7 @@ onMounted(() => {
         <div id="map" class="h-full"></div>
 
         <button @click="toggleBuildings"
-            class="absolute top-4 right-4 bg-white px-4 py-2 rounded-lg shadow-lg hover:bg-gray-100 transition-colors z-10 font-medium">
+            class="absolute top-4 left-4 bg-yellow-300 px-4 py-2 rounded-lg shadow-lg hover:bg-yellow-500 transition-colors z-10 font-medium">
             {{ showBuildings ? 'Hide Buildings' : 'Show Buildings' }}
         </button>
     </div>
